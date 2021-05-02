5ab0efe2086a5edb8792f822c8e36d1cf72e5d10
r-evick <r.evick@hotmail.com>
2021-05-02 02:56:40 -0400

    Update script.js
    added pagination buttons

--- js/script.js
+++ js/script.js
@@ -1,42 +1,33 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+Code by: Ryan Evick
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
+document.addEventListener('DOMContentLoaded', () => {  //allows JS to run no matter where it's placed in HTML
 
 /*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-document.addEventListener('DOMContentLoaded', () => {
 
 function showPage(list, page) {
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9; 
-   const studentList = document.getElementsByClassName('student-list');
-   studentList.innerHTML = "";
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
    
    for (let i = 0; i < list.length; i++) {
-     if (i >= startIndex && i < endIndex) {               //add title,age? which picture size?
+     if (i >= startIndex && i < endIndex) {             
       studentList.innerHTML +=
       `
       <li class="student-item cf">
       <div class="student-details">
-        <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
-        <h3>${list[i].name.title}${list[i].name.first}${list[i].name.last}</h3>
+        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+        <h3>${list[i].name.first} ${list[i].name.last}</h3>
         <span class="email">${list[i].email}</span>  
       </div>
       <div class="joined-details">
-        <span class="date">${list[i].registered.date}</span>
+        <span class="date">Joined ${list[i].registered.date}</span>
       </div>
     </li>
       `;
@@ -50,10 +41,28 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+  const numButtons = parseInt(list.length / 9);
+  const linkList = document.querySelector('.link-list');
+  linkList.innerHTML = '';
+
+  for (let i = 0; i < numButtons; i++) {            
+    linkList.innerHTML +=
+    `
+    <li>
+    <button type="button">${i + 1}</button>
+    </li>
+    `; 
+ }
+}
+
+//Insert the elements you have created to the link-list variable you created earlier. 
+// The insertAdjacentHTML method and beforeend option works well for this.
 
 
 // Call functions
 
-
+showPage(data, 1);
+addPagination(data);
 
 });
