28dd7d7a0929e7d87fab9ae7d9836f4f7c6e0543
r-evick <r.evick@hotmail.com>
2021-05-02 01:11:52 -0400

    Update script.js
    adding initial showPage function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,32 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+document.addEventListener('DOMContentLoaded', () => {
 
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9; 
+   const studentList = document.getElementsByClassName('student-list');
+   studentList.innerHTML = "";
+   
+   for (let i = 0; i < list.length; i++) {
+     if (i >= startIndex && i < endIndex) {               //add title,age? which picture size?
+      studentList.innerHTML +=
+      `
+      <li class="student-item cf">
+      <div class="student-details">
+        <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
+        <h3>${list[i].name.title}${list[i].name.first}${list[i].name.last}</h3>
+        <span class="email">${list[i].email}</span>  
+      </div>
+      <div class="joined-details">
+        <span class="date">${list[i].registered.date}</span>
+      </div>
+    </li>
+      `;
+     }
+   }    
+}
 
 
 /*
@@ -28,3 +53,7 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+
+
+
+});
